15d7d5395565a0756d6d06e2cb5a3b9bacb9c100
Marek Kadek <kadek.marek@gmail.com>
2017-06-20 11:24:47 +0200

    Increment WartRemover version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 private object Dependencies {
   val scalaFmt = "com.geirsson" % "sbt-scalafmt" % "0.6.8"
-  val wartRemover = "org.wartremover" % "sbt-wartremover" % "2.1.0"
+  val wartRemover = "org.wartremover" % "sbt-wartremover" % "2.1.1"
   val dotEnv = "au.com.onegeek" %% "sbt-dotenv" % "1.1.36"
   val coursier = "io.get-coursier" % "sbt-coursier" % "1.0.0-RC3"
 }
