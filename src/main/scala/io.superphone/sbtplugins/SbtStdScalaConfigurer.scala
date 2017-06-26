3505cbfb1ea02b11c26370a226c648b07d266389
Marek Kadek <kadek.marek@gmail.com>
2017-06-26 09:03:38 +0200

    Remove organisation and organisatioName

--- src/main/scala/io.superphone/sbtplugins/SbtStdScalaConfigurer.scala
+++ src/main/scala/io.superphone/sbtplugins/SbtStdScalaConfigurer.scala
@@ -55,8 +55,6 @@ object CommonStatic {
   import scala.Console._
   val shellColors = Array(GREEN, MAGENTA, RED, YELLOW, CYAN)
   val commonSettings: Seq[Setting[_]] = Seq(
-    organization := "io.superphone",
-    organizationName := "Disruptive Multimedia",
     testOptions in Test += Tests.Setup(() => SbtDotenv.trigger),
     sources in (Compile, doc) := Seq.empty,
     updateOptions := updateOptions.value.withCachedResolution(true),
